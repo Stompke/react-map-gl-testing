8baee1ee7a7d999b3f41f11db53d36026144d33a
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-03-23 15:50:54 -0700

    added current location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,41 +1,57 @@
-import React, { useState, PureComponent } from 'react';
+import React, { useState, useEffect, PureComponent } from 'react';
 import ReactMapGL from 'react-map-gl';
-import { SVGOverlay, Marker, FlyToInterpolator } from 'react-map-gl';
+import { SVGOverlay, Marker, FlyToInterpolator, GeolocateControl } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '../App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 const CITIES = [{name: 'test',  longitude: -120.47473814845159, latitude: 47.52488351782222}];
+const dots = [[-120.47473814845159, 47.52488351782222],[-122, 49]];
 
 // PureComponent ensures that the markers are only rerendered when data changes
 class Markers extends PureComponent {
   render() {
     const {data} = this.props;
     return data.map(
-      city => <Marker key={city.name} longitude={city.longitude} latitude={city.latitude} ><h3 className='x'>X</h3></Marker>
+      city => <Marker key={city.name} longitude={city.longitude} latitude={city.latitude} ><button className='x'>X</button></Marker>
     )
   }
 }
 
+let lat = 0;
+let lng = 0;
+
+function success (pos) {
+  lat = pos.coords.latitude;
+  lng = pos.coords.longitude;
+}
+navigator.geolocation.getCurrentPosition(success);
 
 function redraw({project}) {
-    const [cx, cy] = project([-122, 37]);
+  const [cx, cy] = project([lng , lat]);
     return <circle cx={cx} cy={cy} r={4} fill="blue" />;
   }
 
 
+
+
+
 const  Map = () => {
 
-    const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3RvbXBrZSIsImEiOiJjazd4Zm9xdWUwY2kxM2tudDF1aHhxMXFsIn0.DUZcqmMCZB2aLYpi203aUQ';
+    const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3RvbXBrZSIsImEiOiJjazg1MW95amEwMjd4M2hwZmY4M212cHB3In0.H9qLvfx_3y3LLTKRpTffUQ';
 
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
     latitude: 48,
-    // mapStyle: "mapbox://styles/mapbox/dark-v9",
     longitude: -120.4376,
-    zoom: 7,
-    cursor: 'crosshair'
+    mapStyle: "mapbox://styles/mapbox/satellite-v9",
+    zoom: 9,
+    
   });
+
+  const [currentLocation, setCurrentLocation ] = useState({});
 
   const getCoordinates = (e) => {
     console.log(e.lngLat);
@@ -81,6 +97,26 @@ const  Map = () => {
 //     ]
 // });
 
+
+
+  useEffect(() => {
+      function success (pos) {
+        setCurrentLocation({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        })
+      }
+      function error (err) {
+        console.log(err)
+      }
+      function options (options) {
+        console.log(options)
+      }
+    navigator.geolocation.getCurrentPosition(success);
+
+
+  })
+
   return (
       <>
     <ReactMapGL
@@ -88,8 +124,16 @@ const  Map = () => {
       onViewportChange={setViewport}
       mapboxApiAccessToken={MAPBOX_TOKEN}
       onClick={getCoordinates}
+      mapStyle="mapbox://styles/mapbox/satellite-v9"
+      cursor='crosshair'
     //   mapStyle={mapStyle} 
     >
+              <GeolocateControl
+          positionOptions={{enableHighAccuracy: true}}
+          trackUserLocation={true}
+          showUserLocation={true}
+        />
+
         <SVGOverlay redraw={redraw} />
     <Markers data={CITIES} />
     </ReactMapGL>
@@ -97,6 +141,7 @@ const  Map = () => {
     <button onClick={() => goToPlace(47.52488351782222, -120.47473814845159)} >go to Cashmere</button>
     <button onClick={() => goToPlace(48.75687998989363 ,-122.47885352030244)} >go to Bellingham</button>
     <button onClick={() => goToPlace(48.75687998989363 ,-122.48885352030244)} >go to Bellingham2</button>
+    {currentLocation && <button onClick={() => goToPlace(currentLocation.latitude , currentLocation.longitude)} >Current Location</button>}
     </div>
     </>
   );
